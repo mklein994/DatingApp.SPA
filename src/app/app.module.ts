7ac6174c7938ae04940b7dabf1e77d300d71000a
Matthew Klein <mklein994@gmail.com>
2018-05-06 17:51:07 -0500

    Add home and register components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AuthService } from './_services/auth.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
+import { RegisterComponent } from './register/register.component';
 import { ValueComponent } from './value/value.component';
 
 
@@ -15,6 +17,8 @@ import { ValueComponent } from './value/value.component';
     AppComponent,
     ValueComponent,
     NavComponent,
+    HomeComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
