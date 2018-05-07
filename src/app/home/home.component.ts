321a80e895aaea6a06a77484691d887c597e47cb
Matthew Klein <mklein994@gmail.com>
2018-05-06 20:10:23 -0500

    Get user's favourite value on registration

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,14 +9,23 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   registerMode = false;
+  values: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getValues();
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
   }
 
+  getValues() {
+    this.http.get<any>('http://localhost:5000/api/values')
+      .subscribe(values => {
+        console.log(values);
+        this.values = values;
+      });
+  }
 }
