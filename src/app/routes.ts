28bc0026e570d486bb6e115928e7125fdf069bba
Matthew Klein <mklein994@gmail.com>
2018-05-15 21:53:32 -0500

    Add auth guard to members, messages and lists routes

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -7,9 +7,17 @@ import { MemberListComponent } from './member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 
 export const appRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
-  { path: 'messages', component: MessagesComponent },
-  { path: 'lists', component: ListsComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'members', component: MemberListComponent },
+      { path: 'messages', component: MessagesComponent },
+      { path: 'lists', component: ListsComponent },
+    ],
+  },
   { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
