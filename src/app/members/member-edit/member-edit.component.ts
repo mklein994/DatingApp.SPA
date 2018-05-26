1be618cc699d77d7782c4ff612dfcd21cf82c15b
Matthew Klein <mklein994@gmail.com>
2018-05-26 11:22:51 -0500

    Flesh out the edit member template

--- src/app/members/member-edit/member-edit.component.ts
+++ src/app/members/member-edit/member-edit.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 import { User } from '../../_models/user';
-import { ActivatedRoute } from '@angular/router';
+import { AlertifyService } from '../../_services/alertify.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -10,11 +12,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberEditComponent implements OnInit {
   user: User;
+  @ViewChild('editForm') editForm: NgForm;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => this.user = data['user']);
+  }
+
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success('Profile updated successfully');
+    this.editForm.reset(this.user);
   }
 
 }
