29b20bc93f07a74d9cea2f3b4215a78081e3e25f
Matthew Klein <mklein994@gmail.com>
2018-05-29 21:22:39 -0500

    Add loadMessages and pageChanged methods to messages component

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Message } from '../_models/message';
+import { Pagination, PaginatedResult } from '../_models/pagination';
+import { AlertifyService } from '../_services/alertify.service';
+import { AuthService } from '../_services/auth.service';
+import { UserService } from '../_services/user.service';
 
 @Component({
   selector: 'app-messages',
@@ -6,10 +13,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.scss'],
 })
 export class MessagesComponent implements OnInit {
+  messages: Message[];
+  pagination: Pagination;
+  messageContainer: 'Unread';
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private alerify: AlertifyService,
+    private route: ActivatedRoute,
+    public authService: AuthService,
+  ) { }
 
   ngOnInit() {
+    this.route.data.subscribe(
+      data => {
+        this.messages = data['messages'].result;
+        this.pagination = data['messages'].pagination;
+      },
+    );
+  }
+
+  loadMessages() {
+    this.userService
+      .getMessages(this.authService.decodedToken.nameid, this.pagination.currentPage,
+        this.pagination.itemsPerPage, this.messageContainer)
+      .subscribe(
+        (res: PaginatedResult<Message[]>) => {
+          this.messages = res.result;
+          this.pagination = res.pagination;
+        },
+        error => this.alerify.error(error),
+    );
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadMessages();
   }
 
 }
